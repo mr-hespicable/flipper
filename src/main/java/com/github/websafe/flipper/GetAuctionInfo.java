d26feebd6a3ed16b3976c9034dcb33e5a59a2a67
mr-hespicable <85052021+mr-hespicable@users.noreply.github.com>
2023-08-23 00:35:09 +0100

    did stuff

--- src/main/java/com/github/websafe/flipper/GetAuctionInfo.java
+++ src/main/java/com/github/websafe/flipper/GetAuctionInfo.java
@@ -1,5 +1,16 @@
 package com.github.websafe.flipper;
 
 public class GetAuctionInfo {
+    public String GetLBin(String item) {
+        return null;
+    }
+
+    public String GetMedian(String item) {
+        return null;
+    }
+
+    public String GetVolume(String item) {
+        return null;
+    }
 
 }
