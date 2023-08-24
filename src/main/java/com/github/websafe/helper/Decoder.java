6bfe596cbc8fd6cf4006c36a4236eaf256961211
mr-hespicable <85052021+mr-hespicable@users.noreply.github.com>
2023-08-24 11:22:14 +0100

    bada

--- src/main/java/com/github/websafe/helper/Decoder.java
+++ src/main/java/com/github/websafe/helper/Decoder.java
@@ -1,16 +1,20 @@
 package com.github.websafe.helper;
 
 import dev.dewy.nbt.Nbt;
+import dev.dewy.nbt.api.Tag;
 import dev.dewy.nbt.tags.collection.CompoundTag;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Decoder {
     public static final Nbt NBT = new Nbt();
+    private static final File JSON_FILE = new File("samples/sample.json");
 
     public CompoundTag itemBytes(String encoded) throws IOException {
         try {
-            CompoundTag tag = NBT.fromBase64(encoded).toJson(512, );
+            CompoundTag tag = NBT.fromBase64(encoded);
+            NBT.toJson(tag, JSON_FILE);
             return tag;
         } catch (IOException e) {
             throw new RuntimeException(e);
