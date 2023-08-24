eb192bca78a03a8d6b3701b75cf47ae10dedeec8
mr-hespicable <85052021+mr-hespicable@users.noreply.github.com>
2023-08-24 01:12:02 +0100

    started work on making updated itemBytes thingy

--- src/main/java/com/github/websafe/FlipperCommand.java
+++ src/main/java/com/github/websafe/FlipperCommand.java
@@ -8,6 +8,7 @@ import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.ChatComponentText;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Objects;
 public class FlipperCommand extends CommandBase {
@@ -16,7 +17,7 @@ public class FlipperCommand extends CommandBase {
     public static APIRequest api = new APIRequest();
     public static GetAuctionInfo ah = new GetAuctionInfo();
     public static Decoder dec = new Decoder();
-    private static final String temp = "H4sIAAAAAAAAAF1TzZLaRhAedu0YuPiYSlUOSpWPZneQEBKu8kEr5EUsIxZWSEiXLf0M0rAjRKSRAb2Dn4P34Hn8DKkMeFNOcpqu/r7u7+vumjYALdAgbQBA4wpckbjxrQHe6nm1YY02uGZBcg1aIxLjLzRISs76qw3aTy8VpdPdBhdNcGXG4APu4VVPUXBHlUO5I8lx2FH7q35HUqRIlaSuMlD7vO6xyLe4YASXLdBkeM+qApcX6SZ46wS0wo3veJcnpj6GgdulkTRPw6VGzGGeINuDyDYhWkddqzZ2SN89mLpGotH4q5/R0l/QF5NofV5LrNpZ+8N5huz0xV8nEGUW9dbj1BPR3rIXMrItgtYa9IhZ6kRLzM3dIRT9bXjvTD2u+9pnEmW0ju/pn6Eop1Ft1CZ51RTpJsy+wHg5povM2ccuPfju7OIzHo27/tMPXnzv9OKRc/CX6IL9mI3rUWvytOj9N3fmL8flq3Yej+a7KVG//qtHFbq08l3r4Lk+nGQyjfUB9JfpxcekXkie7b+g2qFTdwZRveBzzw5Wvdh59ayHap9YQ622RE+ybANamcN3EPU4XqPMhF49X1suqr21KXqZKSJxvka2RibaT3+h60DPnafxvfF/7+ebsVCc01A3kyk572i/DZ/Mh3/w1Sz/+Y7gw2r2+TO/fBu8i0m5pcGhBd5M8gI3efId+ON0VOyUlEKYM0axwCOcbdnho2AKu3wT4+JCISuBpbi44IGwCw4Cy4UoDYqEpxhockq1vbm54T1/57F+QT4JpyOGp2P/lr8y/AghPBs5HeXTkRqPpt4Eb6wgw+BXnjLOqoKdVmdN4e7ihpt+b+xZEWiMFSSsGC6b568DfrNHC2tozJ9N61l7vpva9sR4NtCj7fGWVcUZH/pQVAZKv9dR+hB2eqtQ7QwiFXaCeCDLMFCVVTxoghYjGS5ZkG3Be/W2q9yKkqB8kiVBQwBcgV+GQRYkGFwD8DfHMEFowAMAAA==";
+    private static final String temp = "CgALaGVsbG8gd29ybGQDAAR0ZXN0AAAAAAA";
 
     @Override
     public int getRequiredPermissionLevel() {
@@ -59,7 +60,11 @@ public class FlipperCommand extends CommandBase {
                         ah.getAuction();
                         break;
                     case "test":
-                        System.out.println(dec.itemBytes(temp));
+                        try {
+                            sender.addChatMessage(new ChatComponentText(dec.itemBytes(temp).toString()));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
                         break;
 
                 }
--- src/main/java/com/github/websafe/helper/Decoder.java
+++ src/main/java/com/github/websafe/helper/Decoder.java
@@ -1,10 +1,19 @@
 package com.github.websafe.helper;
 
 import dev.dewy.nbt.Nbt;
+import dev.dewy.nbt.tags.collection.CompoundTag;
+
+import java.io.IOException;
 
 public class Decoder {
-    public void itemBytes(String encoded) {
+    public static final Nbt NBT = new Nbt();
 
-
+    public CompoundTag itemBytes(String encoded) throws IOException {
+        try {
+            CompoundTag tag = NBT.fromBase64(encoded).toJson(512, );
+            return tag;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
